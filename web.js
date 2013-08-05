98a90648895badc7efbe4d672bb288be4e7eabd7
Ubuntu <ubuntu@ip-10-179-20-35.ec2.internal>
2013-08-05 06:37:38 +0000

    index.html edit for hw4 update1

--- web.js
+++ web.js
@@ -1,13 +1,13 @@
 var fs = require('fs');
 var express = require('express');
 
-var app = express.createServer(express.logger());
+var app = express();//.createServer(express.logger());
 
 app.get('/', function(request, response) {
   response.send(fs.readFileSync('index.html').toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
